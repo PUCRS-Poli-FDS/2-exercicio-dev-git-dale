1ce0dde1bc8238a761ae59ea5d3a1455bc670585
DEIVID FRANCISCO DOS SANTOS <18203069@pucrs.br>
2019-09-03 20:29:41 -0300

    File reader finalizadog

--- src/FileReader.java
+++ src/FileReader.java
@@ -3,40 +3,15 @@ import java.nio.*;
 import java.nio.file.*;
 import java.nio.charset.*;
 import java.util.*;
+import java.nio.file.Files;
 
 public class FileReader{
-    String file = "txt\\mars_commands.txt";
 
-    public FileReader() {
+    private static final String FILE_PATH = "\\txt\\mars_commands.txt";
 
-    }
-
-    public void readFile() {
-
-        String currDir = Paths.get(""). toAbsolutePath().toString();
-        String file = currDir + "\\" + this.file;
-        Path path = Paths.get(file);
-                
-        try (Scanner sc = new Scanner(Files.newBufferedReader(path, Charset.defaultCharset()))) {
-            
-
-
-            String linha = null;
-
-            int mi = Integer.parseInt(sc.nextLine().split("\\s+(\\/\\/ )")[0]);
-            int mf = Integer.parseInt(sc.nextLine().split("\\s+(\\/\\/ )")[0]);
-
-            while (sc.hasNext()) {
-                linha = sc.nextLine();
-                String instrucao = linha.split("\\s+(\\/\\/ )")[0];
-                mem.add(instrucao);
-            }
-            
-            Gerenciador gm = new Gerenciador(mi, mf, mem);
-            System.out.println(gm);
-
-        } catch (IOException e) {
-            System.out.println("Excecao: " + e.getMessage());
-        }
+    public void readFile() throws IOException {
+        String currDir = Paths.get("").toAbsolutePath().toString();
+        File flatFile = new File(currDir + FILE_PATH);
+        List<String> lines = Files.readAllLines(flatFile.toPath());
     }
 }
